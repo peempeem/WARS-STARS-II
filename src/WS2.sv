1e3c96ec678ebaf2b8fb622d489a850822bd212c
peempeem <31141746+peempeem@users.noreply.github.com>
2022-04-14 12:08:56 -0500

    fixed spacing

--- src/WS2.sv
+++ src/WS2.sv
@@ -7,48 +7,48 @@
 
 module WS2 (
 	// Master Clock
-	input					MAX10_CLK1_50,
+	input		logic				MAX10_CLK1_50,
 	
 	// FPGA Input Buttons
-	input		[ 1:0]	KEY,
+	input		logic	[ 1:0]	KEY,
 	
 	// FPGA Input Switches
-	input		[ 9:0]	SW,
+	input		logic	[ 9:0]	SW,
 	
 	// FPGA Leds
-	output 	[ 9:0]	LEDS,
+	output	logic	[ 9:0]	LEDS,
 	
 	// HEX Displays
-	output	[ 7:0]	HEX0,
-							HEX1,
-							HEX2,
-							HEX3,
-							HEX4,
-							HEX5,
+	output	logic	[ 7:0]	HEX0,
+									HEX1,
+									HEX2,
+									HEX3,
+									HEX4,
+									HEX5,
 	
 	// SDRAM
-	output				DRAM_CLK,
-							DRAM_CKE,
-	output	[12:0]	DRAM_ADDR,
-	output	[ 1:0]	DRAM_BA,
-	inout		[15:0]	DRAM_DQ,
-	output				DRAM_LDQM,
-							DRAM_UDQM,
-							DRAM_CS_N,
-							DRAM_WE_N,
-							DRAM_CAS_N,
-							DRAM_RAS_N,
+	output	logic				DRAM_CLK,
+									DRAM_CKE,
+	output	logic	[12:0]	DRAM_ADDR,
+	output	logic	[ 1:0]	DRAM_BA,
+	inout		logic	[15:0]	DRAM_DQ,
+	output	logic				DRAM_LDQM,
+									DRAM_UDQM,
+									DRAM_CS_N,
+									DRAM_WE_N,
+									DRAM_CAS_N,
+									DRAM_RAS_N,
 	
 	// VGA
-	output				VGA_HS,
-	output				VGA_VS,
-	output	[ 3:0]	VGA_R,
-	output	[ 3:0]	VGA_G,
-	output	[ 3:0]	VGA_B,
+	output	logic				VGA_HS,
+	output	logic				VGA_VS,
+	output	logic	[ 3:0]	VGA_R,
+	output	logic	[ 3:0]	VGA_G,
+	output	logic	[ 3:0]	VGA_B,
 	
 	// Arduino
-	inout		[15:0]	ARDUINO_IO,
-	inout					ARDUINO_RESET_N
+	inout		logic	[15:0]	ARDUINO_IO,
+	inout		logic				ARDUINO_RESET_N
 );
 
 	// SPI wires
